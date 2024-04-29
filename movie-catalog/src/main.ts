5491f163bc3c25967ffab9924fd8f8d8677aaa55
pxtheticcat <serega.ivanovv2012@gmail.com>
2024-04-29 17:24:17 +0300

    feat: the scheme for the prism is described
    The relationships for the movie catalog database are described.
    
    Added swagger dependency.

--- movie-catalog/src/main.ts
+++ movie-catalog/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('Movie Catalog Service')
+		.setDescription('API of the movie catalog service')
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
 	await app.listen(3000);
 }
 
